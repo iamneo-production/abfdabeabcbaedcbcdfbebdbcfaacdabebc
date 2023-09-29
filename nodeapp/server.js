e5d888134c20b2d6664515bf6fa26926c4d54005
Iamneo <techadmin@iamneo.ai>
2023-09-29 11:09:43 +0000

    Backup Commit

---
+++ nodeapp/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const PORT = 8080;
+
+// Connect to MongoDB (Make sure MongoDB is running)
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Middleware
+app.use(bodyParser.json());
+app.use('/api', routes);
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
