a179884580e1615082b73f53cadcf557a4385ed0
Iamneo <techadmin@iamneo.ai>
2023-09-29 12:44:23 +0000

    Backup Commit

--- nodeappadv/userController.js
+++ nodeappadv/userController.js
@@ -13,6 +13,7 @@ exports.createUser = async (req, res) => {
 
 exports.getUsers = async (req, res) => {
     try {
+        
         const users = await User.find();
         res.status(200).json(users);
     } catch (error) {
